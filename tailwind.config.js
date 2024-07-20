76630dcbe09cdf9fbeb3e7b4af7805ae83805692
lanre dagogo <lanredagogo23@gmail.com>
2024-07-20 21:39:47 +0100

    deploy quick shop updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./src/**/*.js"],
+  content: ["./build/*/.html", "./src/*/.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
